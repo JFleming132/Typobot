083840c1875b099680216a16e792d8e9b0d0af48
KahootKing132 <josephflemingvi@gmail.com>
2025-08-25 01:39:52 -0400

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -24,11 +24,11 @@ const collectorFilter = (reaction, user) => {
 //Helper function to load the current server's list of allowed words
 async function getDictionary(guildid) {
   try {
-  const result = await Server.findOne({serverid: guildid}, "dictionary");
-  console.log("printing dictionary:")
-  console.log(result.dictionary)
-  //TODO: Add check for if specified server does not exist (and then create it with an empty dictionary)
-  return result.dictionary;
+    const result = await Server.findOne({serverid: guildid}, "dictionary");
+    // console.log("printing dictionary:") //debug message
+    // console.log(result.dictionary) //debug message
+    //TODO: Add check for if specified server does not exist (and then create it with an empty dictionary)
+    return result.dictionary;
   } catch (err) {
     console.error(err)
     return [];
@@ -40,8 +40,8 @@ async function filterWords(msg) {
   try {
   const dictionary = await getDictionary(msg.guild.id); //load dictionary into local array variable
   const check = spell.check(msg.content) //get all mispelled words according to spell-checker.js
-    .filter((word) => !dictionary.includes(word));
-  console.log(check)
+    .filter((word) => !dictionary.includes(word)); //filter out all the words that are in the dictionary
+  //console.log(check) //debug message
   return check; //return final filtered list
   } catch (err) {
     console.error(err)
@@ -50,44 +50,43 @@ async function filterWords(msg) {
 }
 
 module.exports = {
-	name: Events.MessageCreate,
-	once: false,
-	async execute(message) { //upon every message being sent
-    if (message.author.bot) return; //if that message is from this bot, ignore it
+	name: Events.MessageCreate, //what kind of event does this module describe?
+	once: false, //should it run only once or many times?
+	async execute(message) { //execute on ever event of this kind (message being sent)
+    if (message.author.bot) return; //if message is from a bot, ignore it
 		console.log(`Someone sent a message! content: ${message.content}`); //log the message in the debug terminal
     try {
       //TODO: check if message.author.username is a user in the server's document, and if not, create them
-      await Server.findOneAndUpdate({serverid : message.guild.id}, {$inc: {["users" + message.author.username]: 1}}) //increment that users message count by 1
-
-      const check = await filterWords(message)
-
-      console.log(check);
-      if (check !== undefined && check.length != 0) {
+      await Server.findOneAndUpdate({serverid : message.guild.id}, {$inc: {["users" + message.author.username]: 1}}) //increment author's message count by 1
+      const check = await filterWords(message) //remove all words that aren't typos or that are explicitely allowed
+      //console.log(check); //debug message
+      if (check !== undefined && check.length != 0) { //if, after filtering, there are still typos
         const messageContent = check.join(" ") + `\n-# If this message is an error, react with 👎. If enough people react, it will be deleted after 60 seconds. Votes required: ${votesRequired}`;
-        message.channel.send({content: messageContent, reply: {fail_if_not_exists: false, messageReference: `${message.id}`}})
-        .then((m) => {
+        message.channel.send({content: messageContent, reply: {fail_if_not_exists: false, messageReference: `${message.id}`}}) //send the message built above
+        .then((m) => { //once the message is confirmed sent (promise fulfilled)
           //TODO: Log typo in [serverid].users.[user].typos and update [serverid].users.[user].typoCount
           const collector = m.createReactionCollector({filter: collectorFilter, time: 60_000 }); //TODO: Make this timeout a config option
+          //for 60 seconds, collect reactions on the bots message
+          collector.on('collect', (reaction, user) => { //upon receiving a reaction that passes the collectorFilter
+            console.log(`Collected ${reaction.emoji.name} from ${user.tag}`); //debug message
+            m.edit(check.join(" ") + `\n-# If this message is an error, react with 👎. If enough people react, it will be deleted after 60 seconds. Votes required: ${votesRequired - reaction.count}`);
+            if (votesRequired <= reaction.count) { //if enough people have reacted with passing messages
+              m.channel.send({content: "Appeal approved. Deleting message..."}) //notify that the typo is being deleted
+              .then( (m2) => { //once notification is confirmed sent
+                m.delete() //delete the original message
+                m2.delete() //delete the notification
+                //TODO: For each typo, initiate a poll asking if that word should be added to the dictionary
+                //TODO: Reverse the logging of the typo in the users log
+              })
+            }
+          });
 
-            collector.on('collect', (reaction, user) => {
-              console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
-              m.edit(check.join(" ") + `\n-# If this message is an error, react with 👎. If enough people react, it will be deleted after 60 seconds. Votes required: ${votesRequired - reaction.count}`);
-              if (votesRequired <= reaction.count) {
-                m.channel.send({content: "Appeal approved. Deleting message..."})
-                .then( (m2) => {
-                  m.delete()
-                  m2.delete()
-                  //TODO: For each typo, initiate a poll asking if that word should be added to the dictionary
-                })
-              }
-            });
-
-            collector.on('end', collected => {
-              m.edit(check.join(" ")) //after collection ended, disallow reactions.
-            });
+          collector.on('end', collected => { //once the collection ends
+            m.edit(check.join(" ")) //remove the footnote prompting the appeal vote
+          }); 
         })
       }
-    }catch (err) {
+    } catch (err) {
         console.error(err);
     }
 	},
