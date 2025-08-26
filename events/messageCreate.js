85bcfd9e07ac14372287e4160806d1a7e34274e2
KahootKing132 <josephflemingvi@gmail.com>
2025-08-26 01:56:18 -0400

    added comments
    some people actually like to read my code? crazy

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -53,30 +53,41 @@ async function filterWords(msg) {
 module.exports = {
 	name: Events.MessageCreate, //what kind of event does this module describe?
 	once: false, //should it run only once or many times?
+
 	async execute(message) { //execute on ever event of this kind (message being sent)
     if (message.author.bot) return; //if message is from a bot, ignore it
-		console.log(`Someone sent a message! content: ${message.content}`); //log the message in the debug terminal
+    //console.log(`Someone sent a message! content: ${message.content}`); //log the message in the debug terminal
+
     try {
       //TODO: check if message.author.username is a user in the server's document, and if not, create them
       await Server.findOneAndUpdate({serverid : message.guild.id}, {$inc: {["users" + message.author.username]: 1}}) //increment author's message count by 1
       const check = await filterWords(message) //remove all words that aren't typos or that are explicitely allowed
       //console.log(check); //debug message
+      
       if (check !== undefined && check.length != 0) { //if, after filtering, there are still typos
         const messageContent = check.join(" ") + `\n-# If this message is an error, react with 👎. If enough people react, it will be deleted after 60 seconds. Votes required: ${votesRequired}`;
-        message.channel.send({content: messageContent, reply: {fail_if_not_exists: false, messageReference: `${message.id}`}}) //send the message built above
+        message.channel.send({ //Send the message mocking the typo
+          content: messageContent,
+          reply: {
+            fail_if_not_exists: false,
+            messageReference: `${message.id}`
+          }
+        })
         .then((m) => { //once the message is confirmed sent (promise fulfilled)
           //TODO: Log typo in [serverid].users.[user].typos and update [serverid].users.[user].typoCount
-          const collector = m.createReactionCollector({filter: collectorFilter, time: 60_000 }); //TODO: Make this timeout a config option
-          //for 60 seconds, collect reactions on the bots message
-          collector.on('collect', (reaction, user) => { //upon receiving a reaction that passes the collectorFilter
-            console.log(`Collected ${reaction.emoji.name} from ${user.tag}`); //debug message
+          const collector = m.createReactionCollector({ //collect reactions on the bots message
+            filter: collectorFilter, //but only ones that pass the filter
+            time: process.env.APPEAL_TIMEOUT //and only for so long
+          }); 
+          
+          collector.on('collect', (reaction, user) => { //upon collecting a reaction that passes the collectorFilter
+            //console.log(`Collected ${reaction.emoji.name} from ${user.tag}`); //debug message
             m.edit(check.join(" ") + `\n-# If this message is an error, react with 👎. If enough people react, it will be deleted after 60 seconds. Votes required: ${votesRequired - reaction.count}`);
-            if (votesRequired <= reaction.count) { //if enough people have reacted with passing messages
+            if (votesRequired <= reaction.count) { //if enough people have reacted with 👎
               m.channel.send({content: "Appeal approved. Deleting message from database."}) //notify that the typo is being deleted
               .then( (m2) => { //once notification is confirmed sent
-                for (word of check) {
-                  
-                  message.channel.send({
+                for (word of check) { //for each mispelled word
+                  message.channel.send({ //send a message with voting buttons
                     content: `is "${word}" a typo?`,
                     components: [
                       new ActionRowBuilder().addComponents(
@@ -90,21 +101,31 @@ module.exports = {
                           .setStyle('Success')
                       )
                     ]
-                  }).then((pollMessage) => {
-                    const pollCollector = pollMessage.createMessageComponentCollector({ componentType: ComponentType.Button, time: process.env.TYPO_VOTE_TIME});
-                    voteTracker[pollMessage.id] = 0;
-                    pollCollector.on("collect", pollCollected => {
-                      console.log(pollCollected.user)
-                      if (pollCollected.type === 3) {
-                        if (pollCollected.customId === 'typo') {
+                  })
+                  .then((pollMessage) => { //once the message sends,
+                    const pollCollector = pollMessage.createMessageComponentCollector({ //collect button responses 
+                      componentType: ComponentType.Button,
+                      time: process.env.TYPO_VOTE_TIME //but only for a certain time
+                    });
+
+                    voteTracker[pollMessage.id] = {}; //initialize voteTracker entry for this word
+
+                    pollCollector.on("collect", pollCollected => { //whenever the message is interacted with at all
+                      // console.log(pollCollected.user) //debug message
+                      if (pollCollected.type === 3) { //if that interaction was a button being pushed
+                        if (pollCollected.customId === 'typo') { //record the vote appropriately
                           voteTracker[pollMessage.id] = {...voteTracker[pollMessage.id], [pollCollected.user.id]: "Typo"};
                         } else if (pollCollected.customId === 'notTypo') {
                           voteTracker[pollMessage.id] = {...voteTracker[pollMessage.id], [pollCollected.user.id]: "Not a typo"};
                         }
                       }
-                      pollCollected.reply({content: `You voted: ${voteTracker[pollMessage.id][pollCollected.user.id]}`, flags: MessageFlags.ephemeral})
-                    })
-                    pollCollector.on("end", pollCollected => {
+                      pollCollected.reply({
+                        content: `You voted: ${voteTracker[pollMessage.id][pollCollected.user.id]}`,
+                        flags: MessageFlags.Ephemeral
+                      }); //finish interaction with a reply only the voter can see, confirming their vote
+                    });
+
+                    pollCollector.on("end", pollCollected => { //After the timeout has passed on vote time
                       //TODO: Tally votes from voteTracker, clear the relevant word from the object, and manage dictionary appropriately
                       pollMessage.delete()
                       console.log("poll ended")
